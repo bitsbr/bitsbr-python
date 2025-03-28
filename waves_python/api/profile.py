4d71c274cd7604d3872c3d0443c858ed6fc0785a
MDMCoin <110309662+mdmcoin@users.noreply.github.com>
2025-03-28 16:50:08 -0300

    Update profile.py

--- waves_python/api/profile.py
+++ waves_python/api/profile.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class Profile(Enum):
-    MAINNET = "https://nodes.wavesnodes.com"
-    STAGENET = "https://nodes-stagenet.wavesnodes.com"
-    TESTNET = "https://nodes-testnet.wavesnodes.com"
-    LOCAL = "http://127.0.0.1:8081"
+    MAINNET = "https://nodes.mdmcoin.com"
+    STAGENET = "https://nodes-stagenet.mdmcoin.com"
+    TESTNET = "https://nodes-testnet.mdmcoin.com"
+    LOCAL = "http://127.0.0.1:7879"
