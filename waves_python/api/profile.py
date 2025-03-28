baa5a778c4dc1837759718c66aae7f7982e83167
BITSBR <148843115+bitsbr@users.noreply.github.com>
2025-03-28 16:57:42 -0300

    Update profile.py

--- waves_python/api/profile.py
+++ waves_python/api/profile.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class Profile(Enum):
-    MAINNET = "https://nodes.mdmcoin.com"
-    STAGENET = "https://nodes-stagenet.mdmcoin.com"
-    TESTNET = "https://nodes-testnet.mdmcoin.com"
-    LOCAL = "http://127.0.0.1:7879"
+    MAINNET = "https://nodes.bitsbr.org"
+    STAGENET = "https://nodes-stagenet.bitsbr.org"
+    TESTNET = "https://nodes-testnet.bitsbr.org"
+    LOCAL = "http://127.0.0.1:9899"
